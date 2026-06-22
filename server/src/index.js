a448bb3e1880d404e60bb96903424a3e8ed61cd7
Arpit Yadav <arpityadav0506@gmail.com>
2026-06-22 23:50:21 +0530

    initialize express server and mongodb connection

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,32 @@
- 
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json())
+app.use(cookieParser())
+
+export { app }
+
+
+
+import connectDB from "./db/index.js"
+
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server running on port ${process.env.PORT}`)
+        })
+    })
+    .catch((error) => {
+        console.log("Server failed to start:", error)
+    })
